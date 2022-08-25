523a2743ddd1d922680d512fd95adb78c9a4dd0d
Matías Larre Borges <matias@larre-borges.com>
2022-08-25 10:15:14 -0400

    Migrate templates from LEEX to HEEX

--- .formatter.exs
+++ .formatter.exs
@@ -1,5 +1,7 @@
 [
   import_deps: [:ecto, :phoenix],
-  inputs: ["*.{ex,exs}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs}"],
-  subdirectories: ["priv/*/migrations"]
+  plugins: [Phoenix.LiveView.HTMLFormatter],
+  inputs: ["*.{ex,exs,heex}", "priv/*/seeds.exs", "{config,lib,test}/**/*.{ex,exs,heex}"],
+  subdirectories: ["priv/*/migrations"],
+  heex_line_length: 300
 ]
