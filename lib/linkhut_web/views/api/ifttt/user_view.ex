6efbbaae0d2e14f4946d02674e8933dca037d576
Matías Larre Borges <matias@larre-borges.com>
2023-02-01 14:28:54 +0100

    Add view for user related IFTTT operations

--- lib/linkhut_web/views/api/ifttt/user_view.ex
+++ lib/linkhut_web/views/api/ifttt/user_view.ex
@@ -1,8 +1,8 @@
-defmodule LinkhutWeb.Api.IFTTView do
+defmodule LinkhutWeb.Api.IFTT.UserView do
   @moduledoc false
   use LinkhutWeb, :view
 
-  def render("user_info.json", %{name: name, id: id, url: url}) do
-    %{data: %{name: name, id: id, url: url}}
+  def render("info.json", %{user: user, url: url}) do
+    %{data: %{name: user.username, id: user.id, url: url}}
   end
 end
