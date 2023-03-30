406b3b7a754fa2dcd49a140170177c703df93870
Matías Larre Borges <matias@larre-borges.com>
2023-03-30 12:40:22 +0200

    Fix expected output of IFTTT actions

--- lib/linkhut_web/views/api/ifttt/actions_view.ex
+++ lib/linkhut_web/views/api/ifttt/actions_view.ex
@@ -6,38 +6,10 @@ defmodule LinkhutWeb.Api.IFTT.ActionsView do
     %{
       data: [
         %{
-          meta: %{
-            id: id,
-            url: url
-          }
+          id: id,
+          url: url
         }
       ]
     }
-  end
-
-  def render("error.json", %{message: message}) do
-    %{
-      errors: [
-        %{
-          status: "SKIP",
-          message: message
-        }
-      ]
-    }
-  end
-
-  def render("error.json", %{errors: errors}) do
-    render("error.json", %{message: error_message(errors)})
-  end
-
-  defp error_message(errors) do
-    errors
-    |> Enum.map(
-         fn
-           {field, errors} ->
-             "#{Phoenix.Naming.humanize(field)}: #{Enum.join(errors, ", ")}"
-           error -> error
-         end)
-    |> Enum.join(". ")
   end
 end
--- lib/linkhut_web/views/error_view.ex
+++ lib/linkhut_web/views/error_view.ex
@@ -3,6 +3,8 @@ defmodule LinkhutWeb.ErrorView do
 
   use Phoenix.HTML
 
+  alias LinkhutWeb.Api.IFTTT.Errors.BadRequestError
+
   # If you want to customize a particular status code
   # for a certain format, you may uncomment below.
   # def render("500.html", _assigns) do
@@ -11,6 +13,23 @@ defmodule LinkhutWeb.ErrorView do
 
   def render("404.xml", _assigns) do
     "Not Found"
+  end
+
+  def render("400.json", %{conn: conn}) do
+    case conn.assigns.reason do
+      %BadRequestError{message: message} ->
+        %{
+          errors: [
+            %{
+              status: "SKIP",
+              message: message
+            }
+          ]
+        }
+
+      _ ->
+        %{errors: [%{message: "Bad Request"}]}
+    end
   end
 
   # By default, Phoenix returns the status message from
