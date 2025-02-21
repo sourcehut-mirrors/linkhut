f04c7631f2417059e1389c89da25e7a646f1078b
Matías Larre Borges <matias@larre-borges.com>
2025-02-21 16:48:23 +0100

    Fix foreign keys on_delete for imports table

---
+++ priv/repo/migrations/20250221154355_update_fk_imports_table.exs
@@ -0,0 +1,21 @@
+defmodule Linkhut.Repo.Migrations.UpdateFkImportsTable do
+  use Ecto.Migration
+
+  def up do
+    drop constraint(:imports, "imports_user_id_fkey")
+    drop constraint(:imports, "imports_job_id_fkey")
+    alter table(:imports) do
+      modify :user_id, references(:users, on_delete: :delete_all)
+      modify :job_id, references(:oban_jobs, on_delete: :delete_all)
+    end
+  end
+
+  def down do
+    drop constraint(:imports, "imports_user_id_fkey")
+    drop constraint(:imports, "imports_job_id_fkey")
+    alter table(:imports) do
+      modify :user_id, references(:users, on_delete: :nothing)
+      modify :job_id, references(:oban_jobs, on_delete: :nothing)
+    end
+  end
+end
