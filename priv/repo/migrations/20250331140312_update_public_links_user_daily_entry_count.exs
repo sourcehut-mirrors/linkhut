af0fde72281c5ecea2cb7fec8a07df57b50c8be5
Matías Larre Borges <matias@larre-borges.com>
2025-03-31 16:14:30 +0200

    Fix undercounting user daily entries
    Using `rank()` to determine the number of entries a use has created on a
    given day doesn't work when multiple entries were inserted with the same
    insertion timestamp as it counts those as "ties" (meaning they'll share
    the same rank). Using `row_number()` prevents this issue.

---
+++ priv/repo/migrations/20250331140312_update_public_links_user_daily_entry_count.exs
@@ -0,0 +1,50 @@
+defmodule Linkhut.Repo.Migrations.UpdatePublicLinksUserDailyEntryCount do
+  use Ecto.Migration
+
+  def change do
+
+    execute "DROP MATERIALIZED VIEW IF EXISTS public_links;",
+            """
+            CREATE MATERIALIZED VIEW public_links AS
+            SELECT
+               l.id,
+               l.url,
+               l.user_id,
+               l.inserted_at,
+               percent_rank() OVER "reverse_order" AS "rank",
+               rank() OVER "daily_entry" AS "user_daily_entry",
+               count(l.url) OVER "distinct_link" AS "saves",
+               u.username AS "username"
+            FROM links l
+            JOIN users u on l.user_id = u.id
+            WHERE (NOT l.is_private AND u.type != 'unconfirmed' AND NOT l.is_unread AND NOT u.unlisted AND NOT 'via:ifttt' = ANY(l.tags))
+            WINDOW
+            "distinct_link" AS (PARTITION BY l.url),
+            "reverse_order" AS (PARTITION BY l.url ORDER BY l.inserted_at DESC),
+            "daily_entry" AS (PARTITION BY date_trunc('day', l.inserted_at), l.user_id ORDER BY l.inserted_at ASC)
+            ;
+            """
+
+    execute """
+            CREATE MATERIALIZED VIEW public_links AS
+            SELECT
+               l.id,
+               l.url,
+               l.user_id,
+               l.inserted_at,
+               percent_rank() OVER "reverse_order" AS "rank",
+               row_number() OVER "daily_entry" AS "user_daily_entry",
+               count(l.url) OVER "distinct_link" AS "saves",
+               u.username AS "username"
+            FROM links l
+            JOIN users u on l.user_id = u.id
+            WHERE (NOT l.is_private AND u.type != 'unconfirmed' AND NOT l.is_unread AND NOT u.unlisted AND NOT 'via:ifttt' = ANY(l.tags))
+            WINDOW
+            "distinct_link" AS (PARTITION BY l.url),
+            "reverse_order" AS (PARTITION BY l.url ORDER BY l.inserted_at DESC),
+            "daily_entry" AS (PARTITION BY date_trunc('day', l.inserted_at), l.user_id ORDER BY l.inserted_at ASC)
+            ;
+            """,
+            "DROP MATERIALIZED VIEW IF EXISTS public_links;"
+  end
+end
